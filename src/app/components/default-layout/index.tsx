fcde1c985dfad430e9a7352a332dfaba4d1783e7
kuro-xx <k.kuroda@miwaza.co.jp>
2024-05-10 17:59:01 +0900

    1200 px 未満で基本レイアウトを変更

--- src/app/components/default-layout/index.tsx
+++ src/app/components/default-layout/index.tsx
@@ -1,17 +1,34 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { DefaultMenu } from './default-menu';
 import styles from './styles.module.css';
+import { usePathname } from 'next/navigation';
 
 type Props = {
   children: React.ReactNode;
 };
 
 export const DefaultLayout = ({ children }: Props) => {
+  const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => setIsOpen(!isOpen);
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathname]);
+
   return (
     <div className={styles.root}>
       <header className={styles.header}>
         <div className={styles.logo}>Parts List サンプル</div>
         <nav className={styles.navigation}>
-          <DefaultMenu items={links} />
+          <button className={styles.button} onClick={handleClick}>
+            <i className={styles.hamburger} />
+          </button>
+          <div className={`${styles.menu} ${isOpen ? styles.open : ''}`}>
+            <DefaultMenu items={links} />
+          </div>
         </nav>
       </header>
       <div className={styles.main}>
