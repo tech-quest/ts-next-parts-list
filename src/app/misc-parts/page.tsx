46a32a0e1f30f8031cfebc594d7bbc61f804d192
kuro-xx <k.kuroda@miwaza.co.jp>
2024-05-10 16:54:39 +0900

    feature: その他一般パーツを追加

--- src/app/misc-parts/page.tsx
+++ src/app/misc-parts/page.tsx
@@ -1,7 +1,11 @@
 import { PageContainer } from '@/app/shared/components/page-container';
 import { PartsSampleContainer } from '@/app/shared/components/parts-sample-container';
+import { AvatarMedia } from '@/components/containers/media-objects/avatar-media';
 import { Chip } from '@/components/elements/accessories/chip';
+import { FluidImage } from '@/components/elements/images/fluid-image';
 import { Pagination } from '@/components/elements/navigations/pagination';
+import sampleImage960 from './assets/images/sample960x540.png';
+import sampleImage100 from './assets/images/sample100x100.png';
 
 export default function ActionPartsPage() {
   return (
@@ -24,6 +28,31 @@ export default function ActionPartsPage() {
         <PartsSampleContainer>
           <Pagination length={10} />
           <Pagination length={10} offset={2} />
+        </PartsSampleContainer>
+      </section>
+
+      <section>
+        <h2>リキッド画像</h2>
+        <PartsSampleContainer>
+          <FluidImage src={sampleImage960} />
+        </PartsSampleContainer>
+      </section>
+
+      <section>
+        <h2>コメントコンポーネント</h2>
+        <PartsSampleContainer>
+          <AvatarMedia src={sampleImage100}>
+            <p>
+              コメントの本文が入ります。
+              <br />
+              コメントの本文が入ります。コメントの本文が入ります。コメントの本文が入ります。コメントの本文が入ります。
+            </p>
+          </AvatarMedia>
+          <AvatarMedia>
+            <p>
+              プロフィール画像がない場合は、デフォルトの画像が表示されるようにしましょう。
+            </p>
+          </AvatarMedia>
         </PartsSampleContainer>
       </section>
     </PageContainer>
